45535717f4e4774d4b3ab6c3703cd2c443e02fd2
silver13 <silver13@users.noreply.github.com>
2016-04-15 16:47:30 +0930

    Update pid.c

--- OpenMicro/src/pid.c
+++ OpenMicro/src/pid.c
@@ -44,10 +44,10 @@ THE SOFTWARE.
 float pidkp[PIDNUMBER] = {8.0e-2 , 8.0e-2  , 10e-1 }; 
 
 // Ki											ROLL       PITCH     YAW
-float pidki[PIDNUMBER] = { 4e-1  , 4e-1 , 5e-1 };	
+float pidki[PIDNUMBER] = { 6e-1  , 6e-1 , 5e-1 };	
 
 // Kd											ROLL       PITCH     YAW
-float pidkd[PIDNUMBER] = { 4.0e-1 , 4.0e-1  , 5.0e-1 };	
+float pidkd[PIDNUMBER] = { 5.0e-1 , 5.0e-1  , 5.0e-1 };	
 
 // "setpoint weighting" 0.0 - 1.0 where 0.0 = normal pid
 float b[3] = { 0.0 , 0.0 , 0.0};
